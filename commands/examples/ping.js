b02e9b5203e051195dc023be7ffb8590f52bd048
Chad R. Banks <chadrbanks@yahoo.com>
2023-08-12 22:46:47 -0400

    command logs

--- commands/examples/ping.js
+++ commands/examples/ping.js
@@ -5,6 +5,7 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
+        console.log(`${interaction.member.user.username} ran the ping command!`)
 		await interaction.reply('Pong!');
 	},
 };
--- commands/examples/server.js
+++ commands/examples/server.js
@@ -5,6 +5,7 @@ module.exports = {
 		.setName('server')
 		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
+        console.log(`${interaction.member.user.username} ran the server command!`)
 		// interaction.guild is the object representing the Guild in which the command was run
 		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
 	},
